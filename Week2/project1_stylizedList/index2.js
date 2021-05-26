627185a5f6740dc5aae83baca7b82617ac02a21f
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-05-26 17:15:15 -0500

    update2

--- Week2/project1_stylizedList/index2.js
+++ Week2/project1_stylizedList/index2.js
@@ -1,50 +1,52 @@
-const junkWarrior = `
+const junkWarrior = `{"cards":
 [
     {
             "name": "Junk Warrior",
             "type": "Synchro Monster",
             "summoning": "Junk Synchron + 1 or more non-tuners",
-             "desc": "If Synchro Summoned: It gains ATK equal to the total ATK of all Level 2 or lower monsters you currently control",
+             "desc": "If Synchro Summoned: gains ATK equal to the total ATK of all low lv monsters you currently control.",
              "atk": 2300,
             "def": 1300,
             "level": 5,
             "race": "Warrior",
-            "attribute": "DARK",
-            "archetype": "Junk"
+            "attribute": "DARK"
     
 },
     {
         "name": "Junk Synchron",
         "type": "Tuner",
         "summoning": "Normal Summon",
-        "desc": "If normal summoned: special summon one level 2 or below monster from the GY",
+        "desc": "If normal summoned: special summon one level 2 or below monster from the GY.",
         "atk": 1300,
         "def": 500,
         "level": 3,
         "race": "Warrior",
-        "attribute": "Dark",
-        "archetype": "Synchron"
+        "attribute": "Dark"
     },
     {
         "name": "Stardust Dragon",
         "type": "Synchro Monster",
         "summoning": "1 tuner + non-tuner monsters",
-        "desc": "If a card(s) would be destroyed by effect, tribute Stardust Dragon to negate and destroy the effect and card",
+        "desc": "If a card(s) would be destroyed by effect, tribute Stardust Dragon to negate the effect and card.",
         "atk": 2500,
         "def": 2000,
         "level": 8,
         "race": "Dragon",
-        "attribute": "Wind",
-        "archetype": "Stardust"
+        "attribute": "Wind"
     }
-    ]`;
+    ]}`;
 
 const warrior = JSON.parse(junkWarrior);
 var monsterList = document.getElementById("monster-list")
 var output = '';
-for (var i = 0; i < warrior.length; i++) {
-    output += '<li>' + warrior[i].name + '</li>'
+for (var i = 0; i < warrior.cards.length; i++) {
+    output += '<li>' + "Name: " + warrior.cards[i].name + "<br><br>" + "level:" + warrior.cards[i].level + "<br><br>" +
+    "type: " + warrior.cards[i].type + "<br><br>" + "summoning: " + warrior.cards[i].summoning + "<br><br>" + "desc: " + warrior.cards[i].desc +  "<br><br>"
+    + "Atk: " + warrior.cards[i].atk + "<br><br>" + "Def: " + warrior.cards[i].def + "<br><br>" + "race: " + warrior.cards[i].race + "<br><br>" +
+    "attribute: " + warrior.cards[i].attribute
+    
+    '</li>'
     
 }
-var newElement = document.getElementById("monster-list").innerHTML = output;
+monsterList.innerHTML = output;
 console.log(warrior)
