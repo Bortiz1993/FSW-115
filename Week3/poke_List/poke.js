427ba8cf299e59e2273d53c90a02c38a1381a503
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-05-31 04:35:16 -0500

    update2

--- Week3/poke_List/poke.js
+++ Week3/poke_List/poke.js
@@ -1,6 +1,10 @@
 const xhr = new XMLHttpRequest()
+const xhr2 = new XMLHttpRequest()
 xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/", true)
 xhr.send()
+
+xhr2.open("GET", "https://pokeapi.co/api/v2/ability", true)
+xhr2.send()
 
 xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200){
@@ -15,7 +19,26 @@ function showData(data){
     console.log(data)
     for(let i = 0; i < data.length; i++){
         const pokemon = document.createElement('h1')
-        pokemon.textContent = data[i].name
+        pokemon.textContent = "This is the Pokemons name: " + data[i].name
         document.body.appendChild(pokemon)
     }
 }
+
+xhr2.onreadystatechange = function () {
+    if(xhr2.readyState === 4 && xhr2.status === 200){
+        let data = JSON.parse(xhr2.responseText)
+        showData2(data.results)
+    }else if(xhr2.readyState === 4 && xhr2.status !== 200){
+        console.log(xhr2.responseText)
+    }
+}
+
+function showData2(data){
+    console.log(data)
+    for(let i = 0; i < data.length; i++){
+        const pokemon = document.createElement('h1')
+        pokemon.textContent = "This is the ability: " + data[i].name
+        document.body.appendChild(pokemon)
+    }
+}
+
