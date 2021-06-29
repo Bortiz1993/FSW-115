9b3f6543d1e659db9c229f5c6e5bf74a2988374f
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-06-29 18:41:02 -0500

    update3

--- Week6/Axios_chain.js
+++ Week6/Axios_chain.js
@@ -1,43 +1,31 @@
 
-
-// const getData = () => {
-// axios.get('https://swapi.dev/api/people/1')
-// .then(response => {
-//     const homeworldUrl = response.data.homeworld
-//     console.log(homeworldUrl)
-//     return axios.get(homeworldUrl)
-// })
-// .then(response => axios.get(response.data.films[0]))
-// .then(res => console.log(res.data))
-// .catch(err => console.log(err))
-// }
-
-// console.log(axios)
-// getData()
-
-
 const getData = async () => {
     let response;
-   // let homeworld;
+
    let response2;
 
-   let promise;
     try{
          response = await axios.get('https://swapi.dev/api/people/1')
         const homeworld = await axios.get(response.data.homeworld)
-        const lukeFilms = homeworld.data.films
+        const residents = await axios.get(homeworld.data.residents[0])
+        const lukeFilms = await axios.get(homeworld.data.films[0])
+        //test for Promise.all()
         const filmPromises = []
         for(let i = 0; i < lukeFilms.length; i++){
             filmPromises.push(axios.get(lukeFilms[i]))
         }
-        
+
         console.log(response)
         console.log(homeworld)
+        console.log(lukeFilms)
+        console.log(residents)
         //console.log(filmPromises)
-      promise = Promise.all(filmPromises)
-        .then(res => console.log(res))
+         Promise.all(filmPromises)
+
+        .then(response => console.log(response))
         .catch(err => console.log(err))
-        displayData(homeworld, lukeFilms)
+
+        displayData( homeworld, lukeFilms, residents)
     }
     catch(error){
         response2 = await axios.get('https://swapi.dev/api/people/2')
@@ -49,15 +37,18 @@ const getData = async () => {
 
 getData()
 
-function displayData(homeworld, lukeFilms){
-  console.log(homeworld)
-  const getContainer1 = document.getElementById('getContainer')
-  getContainer1.textContent = homeworld.data.name
-  document.body.appendChild(getContainer1)
+function displayData( homeworld, lukeFilms, residents){
 
-  const getContainer2 = document.getElementById('getContainer2')
-  getContainer2.textContent = lukeFilms.data[i].title
+  console.log(homeworld)
+  const getContainer2 = document.getElementById('getContainer1')
+  getContainer2.textContent = homeworld.data.name
   document.body.appendChild(getContainer2)
 
- 
+  const getContainer3 = document.getElementById('getContainer2')
+  getContainer3.textContent = lukeFilms.data.title
+  document.body.appendChild(getContainer3)
+
+ const main = document.getElementById('getContainer')
+ main.textContent = residents.data.name
+ document.body.appendChild(main)
 }
